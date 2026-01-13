368aebbb0b8f5b174f2cb282b52e8f747d9478ac
Nikhil Kumar Tiwari <88082428+nikhilkumar011@users.noreply.github.com>
2026-01-13 14:49:59 +0530

    Implement error handling for empty todo input
    Added error handling for empty todo input with a timeout to clear the error message.

--- frontend/src/components/TodoForm.jsx
+++ frontend/src/components/TodoForm.jsx
@@ -7,9 +7,18 @@ const TodoForm = () => {
 
     const { addTodo } = useTodo();
     const [todoName, SetTodoName] = useState("");
+    const [error, setError] = useState("");
 
     const add = (e) => {
-        if (todoName.trim() === "") return;
+        if (todoName.trim() === "") {
+            setError("Field cannot be empty");
+
+            setTimeout(() => {
+                setError("");
+            }, 2000);
+
+            return;
+        }
         addTodo({
             todo: todoName,
             status: false
@@ -19,24 +28,26 @@ const TodoForm = () => {
     }
 
     const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      add();
-    }
-  };
+        if (e.key === "Enter") {
+            add();
+        }
+    };
 
     return (
-
-        <div className='flex p-4 m-2 gap-5 justify-between'>
-            <input
-                onChange={(e) => { SetTodoName(e.target.value) }}
-                value={todoName}
-                onKeyDown={handleKeyDown} 
-                type="text" placeholder="Enter Your Task" className="w-full border border-gray-400 sm:text-lg py-1 px:4 sm:px-5 rounded text-gray-600"/>
+        <>
+            <div className='flex p-4 m-2 gap-5 justify-between'>
+                <input
+                    onChange={(e) => { SetTodoName(e.target.value) }}
+                    value={todoName}
+                    onKeyDown={handleKeyDown}
+                    type="text" placeholder="Enter Your Task" className="w-full border border-gray-400 sm:text-lg py-1 px:4 sm:px-5 rounded text-gray-600" />
                 <button
                     onClick={add}
                     className="bg-blue-500 text-white rounded cursor-pointer py-2 px-5 hover:bg-blue-700">Add</button>
-        </div>
+            </div>
+            {error && <p style={{ color: "red" }}>{error}</p>}
 
+        </>
 
     )
 }
